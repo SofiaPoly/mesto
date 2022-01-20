9b897995e248eba3ef208b48db0b74cb4e9c49c4
Sofia Polyakova <poly.sofia@yandex.ru>
2022-01-20 20:08:59 +0300

    fix: исправить замечания ревью (2)

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,31 @@
 const buttonProfileEdit = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupOpenedClass = 'popup_opened';
-const buttonClose = document.querySelector('.popup__close-button');
-const buttonSave = document.querySelector('.save-button');
-let formProfile = document.querySelector('.popup__container');
-let formName = formProfile.querySelector('.form__item_type_name');
-let formActivity = formProfile.querySelector('.form__item_type_description');
+const closeButton = document.querySelector('.popup__close-button');
+const saveButton = document.querySelector('.form__save-button');
+let formElement = document.querySelector('.popup__container');
+let nameInput = formElement.querySelector('.form__item_type_name');
+let activityInput = formElement.querySelector('.form__item_type_description');
 let nameProfile = document.querySelector('.profile__name');
 let textProfile = document.querySelector('.profile__text');
 
-function popupOpened() {
-  popup.classList.add('popup_opened');
-  formName.value = nameProfile.textContent;
-  formActivity.value = textProfile.textContent;
+function openPopup() {
+  nameInput.value = nameProfile.textContent;
+  activityInput.value = textProfile.textContent;
+  popup.classList.add(popupOpenedClass);
 };
 
-function popupClose() {
-  popup.classList.remove('popup_opened');
-};
-
-function popupSave() {
-  popup.classList.remove('popup_opened');
+function closePopup() {
+  popup.classList.remove(popupOpenedClass);
 };
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  nameProfile.textContent = formName.value;
-  textProfile.textContent = formActivity.value;
+  nameProfile.textContent = nameInput.value;
+  textProfile.textContent = activityInput.value;
+  closePopup();
 };
 
-formProfile.addEventListener('submit', formSubmitHandler);
-buttonProfileEdit.addEventListener('click', popupOpened);
-buttonClose.addEventListener('click', popupClose);
-buttonSave.addEventListener('click', popupSave);
+formElement.addEventListener('submit', formSubmitHandler);
+buttonProfileEdit.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
