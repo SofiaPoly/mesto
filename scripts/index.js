dbbbba7a6a0be321afeaa35e6237ed89d595d656
Sofia Polyakova <poly.sofia@yandex.ru>
2022-01-26 21:35:54 +0300

    feat: добавить карточки через js

--- scripts/index.js
+++ scripts/index.js
@@ -29,3 +29,55 @@ function formSubmitHandler(evt) {
 formElement.addEventListener('submit', formSubmitHandler);
 buttonProfileEdit.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
+
+const initialCards = [
+  {
+    name: 'Щенок Ричард',
+    link: 'https://images.unsplash.com/photo-1591703291603-2150887a3db5?ixlib.jpg'
+  },
+  {
+    name: 'Щенок Стич',
+    link: 'https://images.unsplash.com/photo-1583337130417-3346a1be7dee?ixlib.jpg'
+  },
+  {
+    name: 'Щенок Винни',
+    link: 'https://images.unsplash.com/photo-1530041539828-114de669390e?ixlib.jpg'
+  },
+  {
+    name: 'Щенок Рокси',
+    link: 'https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?ixlib.jpg'
+  },
+  {
+    name: 'Щенок Френки',
+    link: 'https://images.unsplash.com/photo-1598133894008-61f7fdb8cc3a?ixlib.jpg'
+  },
+  {
+    name: 'Щенок Дейзи',
+    link: 'https://images.unsplash.com/photo-1591856419156-3979c9edd30f?ixlib.jpg'
+  }
+];
+
+const elementContainer = document.querySelector('.elements');
+const cardsTemplate = document.querySelector('#cards-template').content;
+
+const addCards = (element) => {
+  const elementList = cardsTemplate.cloneNode(true);
+  elementList.querySelector('.element__picture').src = element.link;
+  elementList.querySelector('.element__title').textContent = element.name;
+
+  elementContainer.append(elementList);
+}
+
+// const cardSubmitHandler = (evt) => {
+// evt.preventDefault();
+// const card ={
+// name:
+// link:
+// }
+// addCards(card);
+//};
+
+
+initialCards.forEach(element => {
+  addCards(element);
+})
